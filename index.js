ea4a0317a320e437e7006d773114983a2fb4bc93
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-05-21 21:21:54 -0700

    add new ui

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 //    3. Compare the two results
 //    4. Print the Winner (show answers if you want)
 
+// BASE GAME WITHOUT UI
+
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors'];
     let rng = Math.floor(Math.random() * options.length + 1);
@@ -21,8 +23,8 @@ function askUserChoice() {
 }
 
 let rps = (playerChoice, computerChoice) => {
-    let lose = `You lose! You chose ${playerChoice} and the computer chose ${computerChoice}`
-    let win = `You win! You chose ${playerChoice} and the computer chose ${computerChoice}`
+    let lose = `You lose! You chose ${playerChoice} and the computer chose ${computerChoice}`;
+    let win = `You win! You chose ${playerChoice} and the computer chose ${computerChoice}`;
 
     if (playerChoice == computerChoice) return `Draw! Both players chose ${playerChoice}`
     else if (playerChoice == 'rock' && computerChoice == 'paper') return lose;
@@ -33,23 +35,77 @@ let rps = (playerChoice, computerChoice) => {
     else return win;
 }
 
-function game() {
-    let playerScore = 0;
+// function game() {
+//     let playerScore = 0;
+//     let computerScore = 0;
+//     let gamesPlayed = 0;
+
+//     while (gamesPlayed < 5) {
+//         let fullResult = rps(askUserChoice(), getComputerChoice())
+//         let result = fullResult.slice(4,7);
+//         if (result == 'win') playerScore++
+//         else computerScore++
+//         console.log(fullResult)
+//         console.log(`Your score: ${playerScore}`)
+//         console.log(`Cpu score: ${computerScore}`)
+//         gamesPlayed++
+//     }
+
+//     return playerScore > computerScore ? 'Game Over! You win it all!' : 'Game Over! You lose it all!'
+// }
+
+// console.log(game());
+
+
+// Add UI
+
+
+let choices = document.querySelectorAll('.choices');
+let roundResult = document.querySelector('.results');
+let playerScore = 0;
     let computerScore = 0;
+    let gamesPlayed = 0;
 
-    while (playerScore < 5 && computerScore < 5) {
-        let fullResult = rps(askUserChoice(), getComputerChoice())
-        let result = fullResult.slice(4,7);
-        if (result == 'win') playerScore++
-        else computerScore++
-        console.log(fullResult)
-        console.log(`Your score: ${playerScore}`)
-        console.log(`Cpu score: ${computerScore}`)
-    }
+choices.forEach(choice => {
 
-    if (playerScore = 5) console.log('Game Over! You win it all!')
-    else console.log('Game Over! You lose it all!')
-}
+    choice.addEventListener('click', () => {
+     let result = rps(choice.classList[0], getComputerChoice());
+     
+     if (result.slice(4,7) == 'win') playerScore++
+     else if (result.slice(4,7) == 'los') computerScore++
 
-game();
+     roundResult.textContent = result;
+     document.querySelector('.playerScore').textContent = playerScore;
+     document.querySelector('.cpuScore').textContent = computerScore;
+     gamesPlayed++
+
+     if (gamesPlayed == 5 && playerScore > computerScore) document.querySelector('.winner').textContent = `GAME OVER! YOU WIN IT ALL!!!`
+else if (gamesPlayed == 5 && playerScore < computerScore) document.querySelector('.winner').textContent = `GAME OVER! YOU LOSE IT ALL!!!`
+    })
+})
+
+
+
+
+
+    
+    // `You win! You chose ${playerChoice} and the computer chose ${computerChoice}`
+    // `You lose! You chose ${playerChoice} and the computer chose ${computerChoice}`
+    
+    //     let fullResult = rps(askUserChoice(), getComputerChoice())
+    //     let result = fullResult.slice(4,7);
+    //     if (result == 'win') playerScore++
+    //     else computerScore++
+    //     console.log(fullResult)
+    //     console.log(`Your score: ${playerScore}`)
+    //     console.log(`Cpu score: ${computerScore}`)
+    //     gamesPlayed++
+    
+
+    // return playerScore > computerScore ? 'Game Over! You win it all!' : 'Game Over! You lose it all!'
+
+
+
+
+
 
